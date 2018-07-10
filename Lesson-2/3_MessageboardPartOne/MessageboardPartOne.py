2107574b380c7a12b1788fddbaead759091c5ec2
Paulo Ricardo <paulor1809@gmail.com>
2018-07-10 17:05:22 +0000

    Remove console outputs

--- Lesson-2/3_MessageboardPartOne/MessageboardPartOne.py
+++ Lesson-2/3_MessageboardPartOne/MessageboardPartOne.py
@@ -1,22 +1,3 @@
-#!/usr/bin/env python3
-#
-# Step one in building the messageboard server:
-# An echo server for POST requests.
-#
-# Instructions:
-#
-# This server should accept a POST request and return the value of the
-# "message" field in that request.
-#
-# You'll need to add three things to the do_POST method to make it work:
-#
-# 1. Find the length of the request data.
-# 2. Read the correct amount of request data.
-# 3. Extract the "message" field from the request data.
-#
-# When you're done, run this server and test it from your browser using the
-# Messageboard.html form.  Then run the test.py script to check it.
-
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import parse_qs
 
@@ -25,20 +6,16 @@ class MessageHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = self.headers.get('content-length', 0)
         length = int(content_length)
-        print(length)
-        
+
         request_body = self.rfile.read(length)
         data = request_body.decode()
-        print(data)
 
         msg = parse_qs(data)["message"][0]
-        print(msg)
 
-        # Send the "message" field back as the response.
         self.send_response(200)
         self.send_header('Content-type', 'text/plain; charset=utf-8')
         self.end_headers()
-        self.wfile.write(message.encode())
+        self.wfile.write(msg.encode())
 
 if __name__ == '__main__':
     server_address = ('', 8000)
